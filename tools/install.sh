7c6fd030a73d9cd3c4a7620fca734bd144d2358d
greenbreakfast <lazardemin@gmail.com>
2015-12-18 15:06:25 -0500

    added scripts to install library and to create zip file

---
+++ tools/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+## Copy the source code to the Arduino library location 
+
+
+rsync -va Onion/ ~/Documents/Arduino/libraries/Onion/
---
+++ tools/zip.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+## Zip the Arduino Library Code
+
+zip -r Onion-Arduino-Library.zip Onion/
