ddede6f69917db84117b39310ccfeda603a04c6a
gbo@freecision.com <gbo@freecision.com>
2017-05-30 16:05:42 -0400

    removed old files

--- tools/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-## Copy the source code to the Arduino library location 
-
-
-rsync -va Onion/ ~/Documents/Arduino/libraries/Onion/
--- tools/zip.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-## Zip the Arduino Library Code
-
-zip -r Onion-Arduino-Library.zip Onion/
