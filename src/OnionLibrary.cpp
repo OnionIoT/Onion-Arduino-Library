b6789aaa7370b96cc2a7d1c34ba4daea6e3ff8d6
gbo@freecision.com <gbo@freecision.com>
2017-05-30 15:30:48 -0400

    added existing source files into new structure

---
+++ src/OnionLibrary.cpp
@@ -0,0 +1,256 @@
+#include "OnionLibrary.h"
+
+// constructor
+Onion::Onion() 
+{
+	// reset class variables
+	Reset();
+
+	// Arduino Dock setup
+	ArduinoDockSetup();
+}
+
+// destructor
+Onion::~Onion()
+{
+	// nothing for now
+}
+
+// reset function
+void Onion::Reset()
+{
+	neopixelPin				= 0;
+	neopixelStripLength		= 0;
+	neopixelBrightness 		= 0xff;
+	neopixelStrip			= NULL;
+}
+
+
+//// private functions
+// read byte from I2C interface
+//	 return 0 if byte is valid, 1 if byte is invalid
+int Onion::_ReadTwiByte (uint8_t &rdByte) 
+{
+	int status	= EXIT_FAILURE;
+
+	if (Wire.available() > 0) {
+		rdByte	= Wire.read();
+		status	= EXIT_SUCCESS;
+	}
+
+	return status;
+}
+
+int Onion::_ReadTwiByte (int &rdByte) 
+{
+	int status	= EXIT_FAILURE;
+	uint8_t		byte;
+
+	// use the uint8_t version
+	status 	= _ReadTwiByte(byte);
+	rdByte 	= (int)byte;
+
+	return status;
+}
+
+
+//// public functions
+// enable I2C interface and setup ISR
+void Onion::ArduinoDockSetup(uint8_t i2c_addr) {
+	// disable watchdog timer
+	wdt_disable();
+
+	// join i2c bus 
+	Wire.begin(i2c_addr);
+	
+	// register service routine
+	Wire.onReceive(&ReceiveEvent);
+	Wire.onRequest(&RequestEvent);
+}
+
+// Restarts program from beginning but does not reset the peripherals and registers
+void Onion::SoftwareReset() {
+	wdt_enable(WDTO_15MS);
+} 
+
+// perform a specific action based on the i2c address received
+int Onion::ReceiveEventHandler(uint8_t addr)
+{
+	int 	status;
+	int 	i, red, green, blue;
+	uint8_t data;
+
+	switch(addr) {
+		case (ARDUINO_DOCK_ADDR_READBACK):
+			// set the last command to: the address contained in the data
+			status == _ReadTwiByte(data);
+			if (status == EXIT_SUCCESS) {
+				addr 	= data;
+			}
+
+			break;
+
+		case (ARDUINO_DOCK_ADDR_RESET):
+			status == _ReadTwiByte(data);
+			if (status == EXIT_SUCCESS && data == ARDUINO_DOCK_CMD_RESET) {
+				SoftwareReset();
+			}
+
+			break;
+
+		case (ARDUINO_DOCK_ADDR_NEOPIXEL_INIT):
+			status |= _ReadTwiByte(data);
+			if (status == EXIT_SUCCESS && data != 0) {
+				// set the selected pin to output 
+				pinMode(neopixelPin, OUTPUT);
+				// initialize the neopixel object
+				neopixelStrip = new Adafruit_NeoPixel(neopixelStripLength, neopixelPin, NEO_GRB + NEO_KHZ800);
+			}
+			break;
+		 
+		case (ARDUINO_DOCK_ADDR_SET_NEOPIXEL_PIN):
+			status |= _ReadTwiByte(data);
+			if (status == EXIT_SUCCESS) {
+				neopixelPin = data;
+			}
+			break;
+
+		case (ARDUINO_DOCK_ADDR_SET_NEOPIXEL_STRIP_LENGTH):
+			status |= _ReadTwiByte(data);
+			if (status == EXIT_SUCCESS) {
+				neopixelStripLength = data;
+			}
+			break;
+
+		case (ARDUINO_DOCK_ADDR_SET_NEOPIXEL_BRIGHTNESS):
+			status |= _ReadTwiByte(data);
+			if (status == EXIT_SUCCESS) {
+				neopixelBrightness = data;
+
+				// change the brightness
+				if (neopixelStrip != NULL) {
+					neopixelStrip->setBrightness(neopixelBrightness);
+				}
+			}
+			break;
+
+		case (ARDUINO_DOCK_ADDR_SET_NEOPIXEL_DATA):
+			_neopixelAcceptBuffer();
+			break;
+
+		case (ARDUINO_DOCK_ADDR_SET_NEOPIXEL_DATAPOINT):
+			// read pixel index, and colour values
+			status |= _ReadTwiByte(i);
+			status |= _ReadTwiByte(red);
+			status |= _ReadTwiByte(green);
+			status |= _ReadTwiByte(blue);
+
+			// set the datapoint info
+			if (status == EXIT_SUCCESS && neopixelStrip != NULL) {
+				neopixelStrip->setPixelColor(i, red, green, blue);
+			}
+			lastCmdData 	= i;
+			break;
+
+		case (ARDUINO_DOCK_ADDR_SHOW_NEOPIXEL):
+			status |= _ReadTwiByte(data);
+			if (status == EXIT_SUCCESS && data != 0 && neopixelStrip != NULL) {
+				neopixelStrip->show();
+			}
+
+		case (ARDUINO_DOCK_ADDR_DELETE_NEOPIXEL):
+			status |= _ReadTwiByte(data);
+			if (status == EXIT_SUCCESS && data != 0 && neopixelStrip != NULL) {
+				delete neopixelStrip;
+				neopixelStrip 	= NULL;
+				lastCmdData 	= 0xde;
+				// LAZAR: Note: this never executes for some reason...
+			}
+			else {
+				lastCmdData 	= 0xaa;
+			}
+
+		default:
+			break;
+	}
+
+	// note the last address
+	lastCmdAddr 	= addr;
+
+	return status;
+}
+
+int Onion::_neopixelAcceptBuffer ()
+{
+	int 	status;
+	int 	i, length, startPoint;
+	int 	red, green, blue;
+	uint8_t data;
+
+	// read the incoming transmission length and starting index
+	status 	=  _ReadTwiByte(length);
+	status 	|= _ReadTwiByte(startPoint);
+
+	// read a buffer of neopixel pixels (3 bytes)
+	for (i = startPoint; i < (length + startPoint); i++) {
+		if (Wire.available() < 3) {
+			status 	= EXIT_FAILURE;
+			break;
+		}
+
+		red	 	= Wire.read();
+		green 	= Wire.read();
+		blue	= Wire.read();
+
+		neopixelStrip->setPixelColor(i, red, green, blue);
+	}
+	
+	lastCmdData 	= i;
+
+	return status;
+}
+
+// perform a specific action based on the i2c address received
+int Onion::RequestEventHandler(uint8_t &data)
+{
+	int 	status = EXIT_SUCCESS;;
+
+	// respond based on the last addr that was written to
+	switch(lastCmdAddr) {
+		case (ARDUINO_DOCK_ADDR_READBACK):
+			data 	= (uint8_t)lastCmdData;
+			break;
+
+		case (ARDUINO_DOCK_ADDR_RESET):
+			data 	= 0xBE;
+			break;
+
+		case (ARDUINO_DOCK_ADDR_SET_NEOPIXEL_PIN):
+			data 	= (uint8_t)neopixelPin;
+			break;
+
+		case (ARDUINO_DOCK_ADDR_SET_NEOPIXEL_STRIP_LENGTH):
+			data 	= (uint8_t)neopixelStripLength;
+			break;
+
+		case (ARDUINO_DOCK_ADDR_SET_NEOPIXEL_BRIGHTNESS):
+			data 	= (uint8_t)neopixelBrightness;
+			break;
+
+		case (ARDUINO_DOCK_ADDR_DELETE_NEOPIXEL):
+			data 	= (neopixelStrip == NULL ? 0xde : 0x01);
+			break;
+
+		case (ARDUINO_DOCK_ADDR_SET_NEOPIXEL_DATA):
+		case (ARDUINO_DOCK_ADDR_SET_NEOPIXEL_DATAPOINT):
+			data 	= (uint8_t)lastCmdData;
+			break;
+
+		default:
+			status 	= EXIT_FAILURE;
+			data 	= 0xff;
+			break;
+	}
+
+	return status;
+}
---
+++ src/OnionLibrary.h
@@ -0,0 +1,76 @@
+/*
+ * OnionLibrary
+ * Version 0.1 November, 2015
+ * Copyright 2015 Onion Corporation
+
+*/
+
+#ifndef _ONION_LIBRARY_H_
+#define _ONION_LIBRARY_H_
+
+
+#include <Wire.h>
+#include <avr/wdt.h>
+
+#include <Adafruit_NeoPixel.h>
+
+
+#define	REBOOT_STRING                  (0xdead)
+#define	ARDUINO_DOCK_I2C_DEVICE_ADDR   (0x08)
+
+#define ARDUINO_DOCK_ADDR_READBACK		(0x00)
+#define	ARDUINO_DOCK_ADDR_RESET			(0xde)
+#define ARDUINO_DOCK_CMD_RESET	 		(0xad)
+
+#define ARDUINO_DOCK_ADDR_NEOPIXEL_INIT				(0x20)
+#define ARDUINO_DOCK_ADDR_SET_NEOPIXEL_PIN			(0x21)
+#define ARDUINO_DOCK_ADDR_SET_NEOPIXEL_STRIP_LENGTH	(0x22)
+#define ARDUINO_DOCK_ADDR_SET_NEOPIXEL_DATA			(0x23)
+#define ARDUINO_DOCK_ADDR_SET_NEOPIXEL_DATAPOINT	(0x24)
+#define ARDUINO_DOCK_ADDR_SHOW_NEOPIXEL				(0x25)
+#define ARDUINO_DOCK_ADDR_SET_NEOPIXEL_BRIGHTNESS	(0x26)
+#define ARDUINO_DOCK_ADDR_DELETE_NEOPIXEL 			(0x27)
+
+
+// function 
+void 	ReceiveEvent    (int arg);
+void 	RequestEvent    ();
+
+// Onion Library Class
+class Onion {
+public:
+	Onion();
+	~Onion();
+
+	void 	Reset 				(void);
+
+	void 	ArduinoDockSetup	(uint8_t i2c_addr = ARDUINO_DOCK_I2C_DEVICE_ADDR);
+	
+	int 	ReceiveEventHandler		(uint8_t addr);
+	int 	RequestEventHandler		(uint8_t &data);
+	void 	SoftwareReset 			(void);
+	
+	
+
+private:
+
+	int 	_ReadTwiByte 	(uint8_t &rdByte);
+	int 	_ReadTwiByte 	(int &rdByte);
+
+	int 	_neopixelAcceptBuffer ();
+
+
+	// private members
+	int 		lastCmdAddr;
+	int 		lastCmdData;
+
+	int 		neopixelPin;
+	int 		neopixelStripLength;
+	int 		neopixelBrightness;
+	uint8_t 	*neopixelBuffer;
+
+	Adafruit_NeoPixel 	*neopixelStrip;
+
+};
+
+#endif // _ONION_LIBRARY_H_
